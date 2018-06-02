06647caa712e7bb7502688b2b15354170357a3c1
叠加态的猫 <18811474740@163.com>
2018-06-02 17:27:49 +0800

    Add files via upload

--- with_Cython/sample.h
+++ with_Cython/sample.h
@@ -1,1 +1,24 @@
-123
+#ifndef __SAMPLE_H__
+#define __SAMPLE_H__
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int gcd(int x, int y);
+int in_mandel(double x0, double y0, int n);
+int divide(int a, int b, int *remainder);
+double avg(double *a, int n);
+    
+/* A C data structure */
+typedef struct Point {
+    double x,y;
+} Point;
+    
+double distance(Point *p1, Point *p2);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
